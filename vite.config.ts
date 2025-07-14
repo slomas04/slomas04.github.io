70858ad95dfa07e3e35d2e8a13a93a2025950fe3
Samuel Lomas <slomas04@outlook.com>
2025-07-14 12:48:27 +0100

    include snippets as assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({  
+    assetsInclude: ["snippets/*"],
     plugins: [    tailwindcss(),  
 
     ],
@@ -8,9 +9,6 @@ export default defineConfig({
         rollupOptions: {
           input: {
             main: './index.html'
-          },
-          watch: {
-            include: ["snippets/*"]
           }
         }
       }
