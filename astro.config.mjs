1ab4cd680272b09904eadb3583ce64c94c690acf
Sam Lomas <slomas04@outlook.com>
2026-06-24 00:23:43 +0100

    Fonts, classes, tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,65 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders} from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    outDir: 'public',
-    publicDir: 'static',
+  outDir: 'public',
+  publicDir: 'static',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "TerminessMono",
+    cssVariable: "--font-terminess-mono",
+    options:{
+      variants: [{
+        src: ['./src/assets/fonts/TerminessMonoWOFF2/TerminessNFM.woff2'],
+        weight: 'normal',
+        style: 'normal'
+      }, {
+        src: ['./src/assets/fonts/TerminessMonoWOFF2/TerminessNFM-Bold.woff2'],
+        weight: 'bold',
+        style: 'normal'
+      }, {
+        src: ['./src/assets/fonts/TerminessMonoWOFF2/TerminessNFM-Italic.woff2'],
+        weight: 'normal',
+        style: 'italic'
+      }, {
+        src: ['./src/assets/fonts/TerminessMonoWOFF2/TerminessNFM-BoldItalic.woff2'],
+        weight: 'bold',
+        style: 'italic'
+      }]
+    }
+  }, {
+    provider: fontProviders.local(),
+    name: "SegoeUI",
+    cssVariable: "--font-segoeui",
+    options:{
+      variants: [{
+        src: ['/home/sam/git/slomfish-pages/src/assets/fonts/SegoeUI/SegoeUI.woff2'],
+        weight: 'normal',
+        style: 'normal'
+      },{
+        src: ['/home/sam/git/slomfish-pages/src/assets/fonts/SegoeUI/SegoeUI-SemiBold.woff2'],
+        weight: 'bold',
+        style: 'normal'
+      }, {
+        src: ['/home/sam/git/slomfish-pages/src/assets/fonts/SegoeUI/SegoeUI-Bold.woff2'],
+        weight: 'bolder',
+        style: 'normal'
+      }, {
+        src: ['/home/sam/git/slomfish-pages/src/assets/fonts/SegoeUI/SegoeUI-Italic.woff2'],
+        weight: 'normal',
+        style: 'italic'
+      }, {
+        src: ['/home/sam/git/slomfish-pages/src/assets/fonts/SegoeUI/SegoeUI-BoldItalic.woff2'],
+        weight: 'bold',
+        style: 'italic'
+      }]
+    }
+  }],
 });
