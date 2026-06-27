bdb4cbb69306e6cc4601f2f7709b1931d4213257
Samuel Lomas <slomas04@outlook.com>
2026-06-27 23:24:51 +0100

    remove js

---
+++ src/scripts/fadeshader.js
@@ -0,0 +1,5 @@
+const fadeFrag = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+`;
--- src/scripts/videoplayer.js
+++ src/scripts/videoplayer.js
@@ -1,17 +1,10 @@
-import {initColorBuffer} from "./initcolorbuffer.js"
+import {initColorBuffer} from "./initcolorbuffer.js";
+import {bayerFrag} from "./bayerdither.js";
+import {fadeFrag} from "./fadeshader.js";
 
 main()
 
 function main(){
     const canvas = document.getElementById('video-canvas');
     const video  = document.getElementById('background-video');
-
-    const gl = canvas.getContext("webgl");
-    if (gl === null){
-        alert("Your browser failed to start WebGL!!! Whatever man!!");
-        return;
-    }
-
-    
-
 }
