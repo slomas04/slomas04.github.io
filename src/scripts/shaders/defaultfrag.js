48a8c88b1f3dafbab77ba770c2f8634f3b0cfa35
Sam Lomas <slomas04@outlook.com>
2026-06-28 00:44:42 +0100

    Render shader over video elem

---
+++ src/scripts/shaders/defaultfrag.js
@@ -0,0 +1,17 @@
+export const defaultFrag = `
+  #include <common>
+  uniform vec3 iResolution;
+  uniform float iTime;
+  // By iq: https://www.shadertoy.com/user/iq
+  // license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+  
+  void mainImage( out vec4 fragColor, in vec2 fragCoord ){
+    vec2 uv = fragCoord/iResolution.xy;
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+    fragColor = vec4(col,1.0);
+  }
+
+  void main() {
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+  }
+`;
--- src/scripts/videoplayer.js
+++ src/scripts/videoplayer.js
@@ -1,10 +1,73 @@
-import {initColorBuffer} from "./initcolorbuffer.js";
-import {bayerFrag} from "./bayerdither.js";
-import {fadeFrag} from "./fadeshader.js";
+import * as THREE from 'three';
 
-main()
+import {bayerFrag} from "./shaders/bayerdither.js";
+import {defaultFrag} from "./shaders/defaultfrag.js";
+import {defaultVert} from "./shaders/defaultvert.js";
 
-function main(){
-    const canvas = document.getElementById('video-canvas');
-    const video  = document.getElementById('background-video');
+const canvas = document.getElementById('video-canvas');
+const video  = document.getElementById('background-video');
+
+function initShaders(){
+    const renderer = new THREE.WebGLRenderer({canvas});
+    renderer.autoClearColor = false
+    const fragmentShader = bayerFrag;
+
+    const camera = new THREE.OrthographicCamera(
+        -1, // left
+        1, // right
+        1, // top
+        -1, // bottom
+        -1, // near,
+        1, // far
+    );
+    const scene = new THREE.Scene();
+    const plane = new THREE.PlaneGeometry(2, 2);
+
+    const texture = new THREE.VideoTexture(video);
+    texture.colorSpace = THREE.SRGBColorSpace;
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
+
+    const uniforms = {
+        iTime: {value:0},
+        iResolution: {value: new THREE.Vector3()},
+        iChannel0: {value: texture},
+    };
+
+    const material = new THREE.ShaderMaterial({
+        fragmentShader,
+        uniforms,
+    });
+
+    scene.add(new THREE.Mesh(plane, material));
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render(time) {
+        time *= 0.001;  // convert to seconds
+        
+        resizeRendererToDisplaySize(renderer);
+        
+        const canvas = renderer.domElement;
+        uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
+        uniforms.iTime.value = time;
+        
+        renderer.render(scene, camera);
+        
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+
 }
+
+initShaders()
