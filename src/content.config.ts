430ea8420325f808217673423aebba9ac17b3844
Samuel Lomas <slomas04@outlook.com>
2026-07-04 23:41:02 +0100

    Add new content collection for albums

---
+++ src/content.config.ts
@@ -0,0 +1,32 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+// 4. Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
+const albums = defineCollection({
+    loader: file("src/collections/music/albums.json"),
+    schema: z.object({
+        id: z.string(),
+        favourite: z.string(),
+        comment: z.string(),
+        date_added: z.string()
+    })
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { albums };
